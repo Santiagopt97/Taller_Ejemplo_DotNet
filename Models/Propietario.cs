a5df0e053c5b6ddab59d708a5bbbeff63adf436d
Santiagopt97 <santiagopineda99@gmail.com>
2024-09-02 07:24:59 -0500

    Feat: cree conexion entre propietario y vehiculo

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Taller.Models
 {
     public class Propietario
     {
+        [Key]
         public required int Id { get; set; }
         public required string Nombre { get; set; }
         public required string Apellido { get; set; }
--- Models/Vehiculo.cs
+++ Models/Vehiculo.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,19 @@ namespace Taller.Models
 {
     public class Vehiculo
     {
+        [Key]
         public required int Id { get; set; }
         public required string Marca { get; set; }
         public required string Modelo { get; set; }
         public required int Año { get; set; }
         public required string Color { get; set; }
         public required string TipoVehiculo { get; set; }
+        public required int PropietarioId { get; set; } // este va a ser el enlace entre las entidades
+
+
+        //Enlaces foraneos, de esta manera se representan las relaciones entre entidades
+        [ForeignKey("PropietarioId")] //a la llave foranea se le pasa el atributo que es enlace 
+        public Propietario Propietario{ get; set; }
 
         
     }
