937ffc58898c58e264a3d0b0a744463941e20f2b
Santiagopt97 <santiagopineda99@gmail.com>
2024-09-03 06:54:53 -0500

    feat: se agg validaciones a propietario

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -10,11 +10,25 @@ namespace Taller.Models
     {
         [Key]
         public required int Id { get; set; }
+
+        [MinLength(5, ErrorMessage ="El campo nombre debe tener minimo {1} caracteres")]
+        [MaxLength(100, ErrorMessage ="El campo nombre no puede tener mas de {1} caracteres")]
         public required string Nombre { get; set; }
         public required string Apellido { get; set; }
         public required string NumeroIdentificacion { get; set; }
+
+        [Url]
+        public required string FotoPerfil { get; set; }
         public required string Direccion { get; set; }
+
+        [Phone(ErrorMessage ="Usa un formato valido para telefono")]
+        [MinLength(7,ErrorMessage ="minimo 7 caracteres")]
+        [MaxLength(15, ErrorMessage ="Maximo 15 caracteres")]
         public required string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage ="Correo invalido")]
+        [MinLength(5,ErrorMessage ="minimo 5 caracteres")]
+        [MaxLength(255, ErrorMessage ="Maximo 255 caracteres")]
         public required string Correo { get; set; }
     }
 }
--- Readme.md
+++ Readme.md
@@ -1,9 +1,10 @@
-1. primero creamos la carpeta Models y sus clases, en ellas creamos su relacion como se especifica en la clase vehiculo
+1. primero creamos la carpeta Models y sus clases con sus data anotations, en ellas creamos su relacion como se especifica en la clase vehiculo
 2. Creamos la carpeta Data y el db context 
 3. creamos el .env que son las credenciales de la base de datos 
 4. en program.cs cargamos el env con sus variables y creamos la conexion a la base de datos
 5. Hacemos la migracion a la base de datos de la siguiente manera:
-    # Para las migraciones, dotnet ef add migration nombre de la migracion
+    - las migraciones nos sirven para llevar una trazabilidad de la base de datos ya que nos dejan un registro de lo que se modifica en la base de datos
+    # Para las migraciones, dotnet ef add migrations nombre de la migracion
     - pero en este momento la migracion no ha sido efectiva, se puede decir que esta en un stagging hasta que se guarde la migracion
 
     # Para guardar la migracion: dotnet ef database update
